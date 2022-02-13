46d5a955bef992ade60c657fe0ad9e211dc53d8e
mohapatraamisha <99536063+mohapatraamisha@users.noreply.github.com>
2022-02-13 12:31:46 +0530

    Add files via upload

---
+++ arithmetic operations.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter two numbers");
+	scanf("%d %d",&a,&b);
+	printf("the sum is %d\n",a+b);
+	printf("the difference is %d\n",a-b);
+	printf("the product is %d\n",a*b);
+	printf("the quotient is %d\n",a/b);
+	printf("the remainder is %d",a%b);
+	
+}
